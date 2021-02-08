a2b0ccf362cf9bb10e68f017d19eb661d25de6d4
Taylor Page <taylor@thepagesmedia.com>
2021-02-07 20:44:49 -0600

    updated to fix the function

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,27 +1,23 @@
 const sgMail = require('@sendgrid/mail')
 
-export default async function(req, res) {
-  return new Promise ((resolve, reject) => {
+  export default async function(req, res) {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-  const { name, email, message } = req.body
-
-  const msg = {
-    to: 'taylor@thepagesmedia.com',
-    from: 'taylor@thepagesmedia.com',
-    subject: `Dev Site Contact from ${name}`,
-    text: message,
-    html: `<p>Hi, my name is <strong>${name}</strong> and my contact email is: ${email}.</p><p>I want to talk to you about: ${message}</p>`,
+  
+    const { name, email, message } = req.body
+  
+    const content = {
+      to: 'taylor@thepagesmedia.com',
+      from: 'taylor@thepagesmedia.com',
+      subject: `Dev Site Contact from ${name}`,
+      text: message,
+      html: `<p>Hi, my name is <strong>${name}</strong> and my contact email is: ${email}.</p><p>I want to talk to you about: ${message}</p>`
+    }
+  
+    try {
+      await sgMail.send(content)
+      res.status(200).send('Message sent successfully.')
+    } catch (error) {
+      console.log('ERROR', error)
+      res.status(400).send('Message not sent.')
+    }
   }
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent')
-      return resolve();
-    })
-    .catch((error) => {
-      console.error(error)
-      reject(error)
-    })
-  })
-}
