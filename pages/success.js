0b898f2fe9eb57f1e8a195601aacd970a90efb03
Taylor Page <taylor@thepagesmedia.com>
2021-11-07 14:29:58 -0600

    updated robots.txt and also success page to be exluded in indexing

--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 import Layout from "../components/layout";
 import Link from "next/link";
@@ -11,11 +12,17 @@ export default function ContactSuccess() {
   const canonicalURL = site + useRouter().pathname;
   return (
     <Layout success title={siteTitle} siteDesc={siteDesc} keywords={keywords} canonicalURL={canonicalURL}>
-      <section className="prose prose-xl md:prose-2xl border-b border-gray-300 mb-4 pb-4">
-          <h2 className="m-0 p-0 inline">
-          You did it!
-        </h2>
-        <p>Your form was successfully submitted and I will respond within two business days. Feel free to visit the <Link href="/"><a className="go-link">home page</a></Link> or read some more <Link href="/posts"><a className="go-link">blog posts</a></Link>. Thanks for reaching out! I look forward to working together.</p>
+      <Head>
+        <meta name="robots" content="noindex"/>
+      </Head>
+      <section className="prose prose-xl md:prose-2xl border-b border-gray-300 mb-4 pb-4 text-center">
+        <div>
+          <h2 className="m-auto p-0 inline">
+            You did it!
+          </h2>
+        </div>
+        <p>Your form was successfully submitted and I will respond within two business days.</p>
+        <p>Feel free to visit the <Link href="/"><a className="go-link">home page</a></Link> or read some more <Link href="/posts"><a className="go-link">blog posts</a></Link>. Thanks for reaching out! I look forward to working together.</p>
       </section>
     </Layout>
   );
